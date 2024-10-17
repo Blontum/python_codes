ef223593f3729cc3059053c08aba4fe16386284e
blontum <lontumb700@gmail.com>
2024-10-17 16:34:18 -0400

    while looping

---
+++ while-looping.py
@@ -0,0 +1,13 @@
+'''''''''
+In-class Homework
+In-class Homework: 15 mins
+Write a while-loop and inside of your while-loop count down backward from 10 to 0.
+Print starting to countdown backward from 10-0
+Print countdown is over! At the end  '''''''''''''''''''''''
+
+
+m = 10
+while m > 0:
+    print(m)
+    m = m - 1
+print('countdown is over! At the end')
